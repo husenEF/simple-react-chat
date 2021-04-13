b90a0093dc3becdc0463f1477e0590d1ef53c1cd
Husen Efendi <husenef@gmail.com>
2021-04-13 16:04:58 +0700

    clear cache

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,9 +1,15 @@
 import firebase from "firebase";
 
+console.log({ proses: process.env });
 const conf = {
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUHT_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_PROJECTID,
+  measurementId: process.env.REACT_APP_MEASURERMENT_ID,
 };
 
 firebase.initializeApp(conf);
